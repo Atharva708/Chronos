fce682943228c6a55412196d57f43013136ef8e6
Atharva708 <atharvagour07@gmail.com>
2025-02-09 14:32:39 +0530

    Initial Commit

---
+++ Chronos/ChronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronosApp.swift
+//  Chronos
+//
+//  Created by Atharva Gour on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ChronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
